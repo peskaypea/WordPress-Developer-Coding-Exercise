24b38b352a56f5ca591ce4a17b8c65f1ee907a1c
Zane Reid <zanecreid@hotmail.com>
2023-04-12 13:19:05 +0100

    json added php plugin data created

---
+++ rest-reviews.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Rest Reviews
+Description: Fetches reviews from an external REST API and displays them in a list.
+Version: 1.0.0
+Author: Zane 
+Author URI: https://github.com/peskaypea
+*/
